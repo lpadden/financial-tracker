dc3558877dd89086a0db86c11923cbeeb269bd72
Lucas Padden <lpaddendesigns@gmail.com>
2017-07-27 18:11:24 +0000

    Update views and navigation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,11 @@ class User < ActiveRecord::Base
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
   
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonymous"
+  end
+  
   def can_add_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_added?(ticker_symbol)
   end
@@ -19,4 +24,5 @@ class User < ActiveRecord::Base
     return false unless stock
     user_stocks.where(stock_id: stock.id).exists?
   end
+  
 end
